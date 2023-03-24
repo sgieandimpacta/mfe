9cc347e4d24280b046816404f79e675821806d19
sgieandimpacta <gieandes.silva@aluno.faculdadeimpacta.com.br>
2023-03-24 02:33:41 -0300

    feat: renderizando os resultados em tela

--- src/app/components/payments/payment.component.ts
+++ src/app/components/payments/payment.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PaymentService } from 'src/app/Services/payment.service';
+import { Payment } from 'src/app/shared/models/Payment';
 
 @Component({
   selector: 'payment-root',
@@ -8,12 +9,13 @@ import { PaymentService } from 'src/app/Services/payment.service';
 })
 export class PaymentComponent implements OnInit {
   title = 'mfe';
+  payments: Payment[] = [];
   constructor(private service: PaymentService) {}
 
   ngOnInit(): void {
     const payments = this.service.getPayments();
     payments.pipe().subscribe((payments) => {
-      console.log(payments);
+      this.payments = payments;
     });
   }
 }
