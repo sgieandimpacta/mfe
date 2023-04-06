7ab5c6420b750df97d22d1682b1d9f8c9aa36bdc
sgieandimpacta <128334003+sgieandimpacta@users.noreply.github.com>
2023-04-06 10:17:41 -0300

    fix: definindo o currency br global (#15)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import ptBr from '@angular/common/locales/pt';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CurrencyMaskModule } from 'ng2-currency-mask';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CompanyComponent } from './components/companies/company.component';
+import { CreateCompanyComponent } from './components/companies/create/create-company.component';
+import { ListCompanyComponent } from './components/companies/list/list-company.component';
 import { CreatePaymentComponent } from './components/payments/create/create-payment.component';
 import { DetailPaymentComponent } from './components/payments/detail/detail-payment.component';
 import { EditPaymentComponent } from './components/payments/edit/edit-payment.component';
 import { ListPaymentComponent } from './components/payments/list/list-payment.component';
 import { PaymentComponent } from './components/payments/payment.component';
-import { CreateCompanyComponent } from './components/companies/create/create-company.component';
-import { ListCompanyComponent } from './components/companies/list/list-company.component';
 registerLocaleData(ptBr);
 
 const components = [
@@ -42,7 +41,10 @@ const components = [
     BrowserAnimationsModule,
     NgbModule,
   ],
-  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
